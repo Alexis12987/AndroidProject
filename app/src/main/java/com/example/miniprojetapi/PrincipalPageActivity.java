e87630a26a37a33b99a03b47cd4898279d90629c
alexis coinet <alexiscoinet12@gmail.com>
2019-12-03 12:19:46 +0100

    fix

---
+++ app/src/main/java/com/example/miniprojetapi/PrincipalPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.miniprojetapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_page);
+    }
+}
