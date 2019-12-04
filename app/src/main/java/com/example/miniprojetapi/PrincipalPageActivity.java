6b98c1295e491e109d923f769e3d0362d91ee0c5
Alexis Coinet <alexis.coinet@etu.univ-nantes.fr>
2019-12-04 13:27:57 +0100

    add main page

--- app/src/main/java/com/example/miniprojetapi/PrincipalPageActivity.java
+++ app/src/main/java/com/example/miniprojetapi/PrincipalPageActivity.java
@@ -2,7 +2,10 @@ package com.example.miniprojetapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PrincipalPageActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class PrincipalPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_page);
+        Button startBtn = (Button) findViewById(R.id.startbtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(PrincipalPageActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
